4693147b3c8d147c97c6fc9c281ff264a8c5a920
quicksilver812 <allen.20211cse0227@presidencyuniversity.in>
2024-06-16 14:35:43 +0530

    authentication functionality added

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,26 @@
+require('dotenv').config()
+
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
-});
+    const { username } = req.body;
+    // Get or create user on Chat Engine!
+    try {
+      const r = await axios.put(
+        "https://api.chatengine.io/users/",
+        { username: username, secret: username, first_name: username },
+        { headers: { "Private-Key": process.env.PRIVATE_KEY } }
+      );
+      return res.status(r.status).json(r.data);
+    } catch (e) {
+      return res.status(e.response.status).json(e.response.data);
+    }
+  });
 
 app.listen(3001);
